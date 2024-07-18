f5be7c246cf53a2cb4ca114d9315e3f3b6dad159
nick-bugxhunter <138686129+nick-bugxhunter@users.noreply.github.com>
2024-07-18 15:08:32 +1000

    Update main.py

--- main.py
+++ main.py
@@ -180,15 +180,10 @@ with st.sidebar:
     st.markdown(
         "Threat modelling is a key activity in the software development lifecycle, but is often overlooked or poorly executed. THREATMOD GPT aims to help teams produce more comprehensive threat models by leveraging the power of Large Language Models (LLMs) to generate a threat list, attack tree and/or mitigating controls for an application based on the details provided."
     )
-    st.markdown("Modified by [Nick Forshteyn](https://www.linkedin.com/in/forstein/).")
+    st.markdown("Created by [Nick Forshteyn](https://www.linkedin.com/in/forstein/).")
     # Add "Star on GitHub" link to the sidebar
     st.sidebar.markdown(
         "⭐ Star on GitHub: [![Star on GitHub](https://img.shields.io/github/stars/nick-bugxhunter/threatmodgpt?style=social)](https://github.com/nick-bugxhunter/threatmodgpt)"
-    )
-    st.markdown("Based on original work by [Matt Adams](https://www.linkedin.com/in/matthewrwadams/).")
-    # Add "Star on GitHub" link to the sidebar
-    st.sidebar.markdown(
-        "⭐ Star on GitHub: [![Star on GitHub](https://img.shields.io/github/stars/mrwadams/stride-gpt?style=social)](https://github.com/mrwadams/stride-gpt)"
     )
     st.markdown("""---""")
 
